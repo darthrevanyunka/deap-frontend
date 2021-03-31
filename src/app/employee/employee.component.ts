958465c2d93e3563073a924570cd3323cf7d13c7
Hanno.vdWalt <hanno>
2021-03-31 16:01:36 +0200

    Added typescript to send JSON to endpoint

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-employee',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  onCreateEmployee(employeeForm){
+    //put form values into variables
+    const formEmployee = employeeForm.value;
+    const email = formEmployee.email;
+    const firstName = formEmployee.firstName;
+    const lastName = formEmployee.lastName;
+    const password = formEmployee.password;
+    const confPassword = formEmployee.confirmPassword;
+
+    //check whether passwords match
+    if(password == confPassword){
+
+        //create employee object
+        const employee = {
+          email: email,
+          firstName: firstName,
+          lastName: lastName,
+          password: password
+        }
+
+        //convert employee object to JSON
+        const employeeJson = JSON.stringify(employee);
+
+        //loggers
+        console.log(employee);
+        console.log(employeeJson);
+
+        //post request to create employee
+        this.http.post('localhost/api/adduser',
+        employeeJson
+        ).subscribe(responseData => {
+            console.log(responseData);
+        });
+
+    }else{
+      console.log("Passwords do not match");
+      alert("Passwords do not match");
+    }
+  }
 }
+
