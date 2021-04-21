b990955a37dfdfa808362ec78f363882d23b81bc
hannovdw <hannovdw1998@gmail.com>
2021-04-21 11:58:50 +0200

    improved UI to only show admin page after successfull login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
+import { AppComponent } from './app.component';
+
 
 const routes: Routes = [
+  {path: 'admin', component: EmployeeComponent},
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { OAuthService, NullValidationHandler, AuthConfig } from 'angular-oauth2-oidc';
 import { JwksValidationHandler } from 'angular-oauth2-oidc';
-
+import {Router} from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -11,26 +12,32 @@ import { JwksValidationHandler } from 'angular-oauth2-oidc';
 export class AppComponent {
   title = 'deap-front-end';
 
-  constructor(private oauthService: OAuthService) {
+  constructor(private oauthService: OAuthService, private router: Router) {
       this.configure();
   }
 
+  currentApplicationVersion = environment.appVersion;
+  logedIn = true;
+
+
   authConfig: AuthConfig = {
       issuer: 'http://localhost:8080/auth/realms/SpringBootKeycloak',
-      redirectUri: window.location.origin + "/",
+      redirectUri: window.location.origin + "/admin",
       clientId: 'spa-employee',
       responseType: 'code',
-      // at_hash is not present in JWT token
       disableAtHashCheck: true,
       showDebugInformation: true
     }
 
     public login() {
       this.oauthService.initLoginFlow();
+      this.logedIn = true;
     }
 
     public logoff() {
       this.oauthService.logOut();
+      this.router.navigate(['']);
+      this.logedIn = false;
     }
 
     private configure() {
@@ -38,6 +45,4 @@ export class AppComponent {
       this.oauthService.tokenValidationHandler = new  NullValidationHandler();
       this.oauthService.loadDiscoveryDocumentAndTryLogin();
     }
-
-
 }
